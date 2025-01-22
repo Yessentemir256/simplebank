bce5b803b9ea94b913e1e59f8ef572c905cab432
Yessentemir <yessentemir.utepov@gmail.com>
2025-01-23 02:37:00 +0500

    add store.go and store_test.go

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,12 +1,14 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 )
 
 // Store provides all functions to execute db queries and transactions
 type Store struct {
-	*Querier
+	*Queries
 	db *sql.DB
 }
 
@@ -49,4 +51,46 @@ type TransferTxResult struct {
 	ToAccount   Account  `json:"to_account"`
 	FromEntry   Entry    `json:"from_entry"`
 	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TransferTx performs a money transfer from one account to the other.
+// It creates the transfer, add account entries, and update accounts' balance within a database transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: update accounts' balance
+
+		return nil
+
+	})
+
+	return result, err
 }
