21ba92c8b5dc44dd7ca48887ad0b0806c741bb9c
Yessentemir <yessentemir.utepov@gmail.com>
2025-02-08 10:49:36 +0500

    added config

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"log"
+
 	"github.com/Yessentemir256/simplebank/api"
 	db "github.com/Yessentemir256/simplebank/db/sqlc"
+	"github.com/Yessentemir256/simplebank/util"
 	_ "github.com/lib/pq"
-	"log"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load configuration", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -23,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 
